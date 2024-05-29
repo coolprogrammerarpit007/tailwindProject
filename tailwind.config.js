3a3270cfd8d0878fbb7546f57bc8dff553a64ff0
Arpit <arpit.mishra.out@gmail.com>
2024-05-29 19:20:13 +0530

    tailwind basics

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+`use strict`;
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [`./index.html`],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EE0081",
+        secondary: "green",
+      },
+    },
+  },
+  plugins: [],
+};
